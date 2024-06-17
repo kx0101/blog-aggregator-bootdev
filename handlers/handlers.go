98b88eda5b88df0490fc40801c8b6a894a83c323
kx0101 <liakos.koulaxis@yahoo.com>
2024-06-17 15:55:06 +0300

    .

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,8 +1,14 @@
 package handlers
 
 import (
-	"github.com/kx0101/blog-aggregator-bootdev/utils"
+	"encoding/json"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/kx0101/blog-aggregator-bootdev/internal/database"
+	"github.com/kx0101/blog-aggregator-bootdev/utils"
 )
 
 func handleHealthz(w http.ResponseWriter, _r *http.Request) {
@@ -12,14 +18,46 @@ func handleHealthz(w http.ResponseWriter, _r *http.Request) {
 		Status: "ok",
 	}
 
-	utils.RespondWithJSON(w, 200, responseBody)
+	utils.RespondWithJSON(w, http.StatusOK, responseBody)
 }
 
 func handleErr(w http.ResponseWriter, _r *http.Request) {
-	utils.RespondWithError(w, 500, "Internal Server Error")
+	utils.RespondWithError(w, http.StatusInternalServerError, "Internal Server Error")
 }
 
-func RegisterHandlers(mux *http.ServeMux) {
+func handleCreateUser(w http.ResponseWriter, r *http.Request, dbQueries *database.Queries) {
+	var requestBody struct {
+		Name string `json:"name"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&requestBody); err != nil {
+		log.Printf("Error: %s", err)
+		utils.RespondWithError(w, http.StatusBadRequest, "Invalid request payload")
+		return
+	}
+
+	id := uuid.New()
+	now := time.Now()
+
+	user, err := dbQueries.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        id,
+		CreatedAt: now,
+		UpdatedAt: now,
+		Name:      requestBody.Name,
+	})
+	if err != nil {
+		log.Printf("Error inserting new user: %s", err)
+		utils.RespondWithError(w, http.StatusInternalServerError, "Error creating user")
+		return
+	}
+
+	utils.RespondWithJSON(w, http.StatusOK, user)
+}
+
+func RegisterHandlers(mux *http.ServeMux, dbQueries *database.Queries) {
 	mux.HandleFunc("/v1/healthz", handleHealthz)
 	mux.HandleFunc("/v1/err", handleErr)
+	mux.HandleFunc("/v1/users", func(w http.ResponseWriter, r *http.Request) {
+		handleCreateUser(w, r, dbQueries)
+	})
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,7 +9,13 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/kx0101/blog-aggregator-bootdev/handlers"
+	"github.com/kx0101/blog-aggregator-bootdev/internal/database"
+	_ "github.com/lib/pq"
 )
+
+type apiConfig struct {
+	DB *database.Queries
+}
 
 func main() {
 	err := godotenv.Load()
@@ -16,13 +23,21 @@ func main() {
 		log.Printf("Error: %s", err)
 	}
 
+	dbURL := os.Getenv("dbURL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatalf("Error: %s", err)
+	}
+
+	dbQueries := database.New(db)
+
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
 		log.Fatalf("PORT environment variable not set")
 	}
 
 	mux := http.NewServeMux()
-	handlers.RegisterHandlers(mux)
+	handlers.RegisterHandlers(mux, dbQueries)
 
 	server := &http.Server{
 		Addr:    fmt.Sprintf(":%v", PORT),
