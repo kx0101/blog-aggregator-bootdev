2d99f3ab14d70a63ccaa0f374075f29bcd51a6b5
kx0101 <liakos.koulaxis@yahoo.com>
2024-06-27 23:11:57 +0300

    fixes

--- utils/worker.go
+++ utils/worker.go
@@ -36,7 +36,7 @@ func FeedWorker(dbQueries *database.Queries, interval time.Duration, batchSize i
 		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 
-		feeds, err := dbQueries.GetNextFeedsToFetch(ctx, 10)
+		feeds, err := dbQueries.GetNextFeedsToFetch(ctx, int32(batchSize))
 		if err != nil {
 			fmt.Printf("Error fetching RSS feed %v: %s\n", feeds, err)
 			return
@@ -82,8 +82,6 @@ func FeedWorker(dbQueries *database.Queries, interval time.Duration, batchSize i
 		}
 
 		wg.Wait()
-
-		time.Sleep(interval)
 	}
 }
 
